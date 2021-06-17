3f6bbf402c49892b3311d2acf7f8af2a4ef259cf
Gabriel872 <54552055+Gabriel872@users.noreply.github.com>
2021-06-17 15:47:58 -0300

    0.3.5
    - Create new section component for skills list;

---
+++ js/remove.js
@@ -0,0 +1,14 @@
+// var navbar = document.querySelector("header");
+
+// navbar.remove();
+
+// $(document).ready(function() {
+//     $("div#extraControls").removeClass("header");
+// });
+
+//replace element content by class or id
+
+/* <script>
+var demo = document.querySelector(".demo");
+demo.textContent = "aaaaaaaaaaaaaaaaaaaaaaa";
+</script> */
