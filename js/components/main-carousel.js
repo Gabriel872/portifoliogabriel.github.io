a05c6cc21e06235111f9c1217a4b281daa1a55b6
Gabriel872 <54552055+Gabriel872@users.noreply.github.com>
2021-06-28 20:11:24 -0300

    0.6
    - Add section "About";

---
+++ js/components/main-carousel.js
@@ -0,0 +1,22 @@
+
+const btnRight = document.querySelector('.button-arrow.-right');
+const btnLeft = document.querySelector('.button-arrow.-left');
+const elements = document.querySelector('.elements');
+
+var i = 0;
+
+btnRight.addEventListener('click', function(){
+    i += 200;
+    elements.style = 
+    `
+        transform: translateX(${i}px)
+    `;
+});
+
+btnLeft.addEventListener('click', function(){
+    i -= 200;
+    elements.style = 
+    `
+        transform: translateX(${i}px)
+    `;
+});
