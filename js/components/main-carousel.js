dca52ca1c54496f6087200407ad0bc543f93dc72
Gabriel Bublitz <gabriel.bublitz@hotmail.com>
2022-11-06 10:52:40 -0300

    1.1.0
    - Update curriculum.

--- js/components/main-carousel.js
+++ js/components/main-carousel.js
@@ -1,22 +1,22 @@
 
-const btnRight = document.querySelector('.button-arrow.-right');
-const btnLeft = document.querySelector('.button-arrow.-left');
-const elements = document.querySelector('.elements');
+// const btnRight = document.querySelector('.button-arrow.-right');
+// const btnLeft = document.querySelector('.button-arrow.-left');
+// const elements = document.querySelector('.elements');
 
-var i = 0;
+// var i = 0;
 
-btnRight.addEventListener('click', function(){
-    i += 200;
-    elements.style = 
-    `
-        transform: translateX(${i}px)
-    `;
-});
+// btnRight.addEventListener('click', function(){
+//     i += 200;
+//     elements.style = 
+//     `
+//         transform: translateX(${i}px)
+//     `;
+// });
 
-btnLeft.addEventListener('click', function(){
-    i -= 200;
-    elements.style = 
-    `
-        transform: translateX(${i}px)
-    `;
-});
+// btnLeft.addEventListener('click', function(){
+//     i -= 200;
+//     elements.style = 
+//     `
+//         transform: translateX(${i}px)
+//     `;
+// });
